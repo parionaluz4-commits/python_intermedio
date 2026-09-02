5938a0a57ed383218c2f77ed562e4a4028519146
parionaluz4-commits <parionaluz4@gmail.com>
2026-09-02 08:42:41 -0500

    clase final de condicionales
    clase sobre el manejo para trabajar el if

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-temp:int=20
-if temp>35:
-  print("temperatura alta")
-else:
-   print("temperatura normal") 
+
+vocal:str=input("infrese la letra: ")
+match vocal:
+    case"a"|"e"|"i"|"o"|"u"
+     print("es una vocal")
+    case _:
+     print("es una consonante")
